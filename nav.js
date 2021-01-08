7b83d9854ec97d26863521f91494ebc4324e03df
Maximilian Bethke <maxbethke@web.de>
2021-01-08 16:12:41 +0100

    Optimized menu for small screen sizes

--- nav.js
+++ nav.js
@@ -1,12 +1,41 @@
 
 var init = () => {
     $(".card").on("click", cardToggleBody)
+    $(".menu__toggle, .menu__blackout").on("click", menuToggle)
 }
 
 var cardToggleBody = (e) => {
-    console.log("QQ")
+    console.log("Card:Toggle")
     ele = e.target;
     $(ele).children(".card__body").slideToggle();
+}
+
+var menuOpen = false
+
+var menuToggle = () => {
+    console.log("Menu:Toggle");
+    $(".menu__blackout").fadeToggle();
+
+    $(".menu__toggle").fadeOut({
+        complete: () => {
+            $(".menu__toggle").toggleClass("fa-bars fa-times");
+            $(".menu__toggle").fadeIn();
+        }
+    });
+
+    if(menuOpen) { // close menu
+        $(".menu__collapse").css({
+            left: "-70vw",
+            right: "100vw"
+        });
+    } else { // open menu
+        $(".menu__collapse").css({
+            left: "0",
+            right: "30vw"
+        });
+    }
+
+    menuOpen = !menuOpen;
 }
 
 $(document).ready(init);
