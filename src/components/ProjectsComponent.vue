dbee18ae4f9bc3ae7de28804c28cc55f4e090cd8
Maximilian Bethke <maxbethke@web.de>
2022-06-23 18:18:30 +0200

    ME-10: loading animation

--- src/components/ProjectsComponent.vue
+++ src/components/ProjectsComponent.vue
@@ -1,23 +1,30 @@
 <template>
-  <v-container>
-    <div v-if="projects.length > 0" class="projects">
-      <div
-          v-for="item in projects"
-          :key="item._id"
-      >
-        <ProjectComponent :project="item"/>
+  <div>
+    <v-progress-linear
+        v-show="isLoading"
+        indeterminate
+        color="primary"
+    ></v-progress-linear>
+    <v-container>
+      <div v-if="projects.length > 0" class="projects">
+        <div
+            v-for="item in projects"
+            :key="item._id"
+        >
+          <ProjectComponent :project="item"/>
+        </div>
       </div>
-    </div>
-    <v-alert v-if="projects.length < 1 &&! isLoading &&! loadingError" type="info">
-      No projects found... thats odd
-    </v-alert>
-    <v-alert v-if="isLoading" type="info">
-      Loading projects...
-    </v-alert>
-    <v-alert v-if="loadingError" type="error">
-      There was an error loading my projects.
-    </v-alert>
-  </v-container>
+      <v-alert v-if="projects.length < 1 &&! isLoading &&! loadingError" type="info">
+        No projects found... thats odd
+      </v-alert>
+      <v-alert v-if="isLoading" type="info">
+        Loading projects... this could take a while
+      </v-alert>
+      <v-alert v-if="loadingError" type="error">
+        There was an error loading my projects.
+      </v-alert>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -47,8 +54,14 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+@import '~vuetify/src/styles/settings/_variables'
+
 .projects
   display: grid
   grid-template-columns: 1fr 1fr 1fr
   grid-gap: 1em
+
+@media #{map-get($display-breakpoints, 'sm-and-down')}
+  .projects
+    grid-template-columns: 1fr
 </style>
