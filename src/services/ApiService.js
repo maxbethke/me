856b395214dbfbc535964f626e1259b1eae9a6e4
Maximilian Bethke <maxbethke@web.de>
2022-06-24 06:49:40 +0200

    ME-13: basic auth

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,27 +1,25 @@
 import axios from 'axios'
 
-class ApiService {
-    #NOTION_ENDPOINT = process.env.VUE_APP_NOTION_ENDPOINT
+const NOTION_ENDPOINT = process.env.VUE_APP_NOTION_ENDPOINT
 
-    queryNotion(database) {
-        return this.#request(this.#NOTION_ENDPOINT+'/'+database)
-    }
-
-    async #request(
-        url,
-        method = 'get'
-    ) {
-        try {
-            const response = await axios({
-                url: url,
-                method: method
-            });
-            return response.data;
-        } catch (error) {
-            console.error('Error performing API Request', error);
-            throw new Error('API Request failed')
-        }
+const request = async (
+    url,
+    method = 'get'
+) => {
+    try {
+        const response = await axios({
+            url: url,
+            method: method,
+            auth: {
+                username: process.env.VUE_APP_NOTION_API_USER,
+                password: process.env.VUE_APP_NOTION_API_SECRET
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error performing API Request', error);
+        throw new Error('API Request failed')
     }
 }
 
-export default new ApiService()
+export const queryNotion = database => request(NOTION_ENDPOINT+'/'+database)
