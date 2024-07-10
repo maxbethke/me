d31aaa27cd8ab8171a88ddb8138941c22db70291
Maximilian Bethke <office@maxbethke.de>
2024-07-10 14:28:44 +0200

    Add shadcn button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import {Button} from "@/components/ui/button";
 
 const pageStyles = {
   color: "#232129",
@@ -144,10 +145,8 @@ const IndexPage: React.FC<PageProps> = () => {
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
+      <Button>Button</Button>
+      <p>Hi</p>
       <ul style={doclistStyles}>
         {docLinks.map(doc => (
           <li key={doc.url} style={docLinkStyle}>
